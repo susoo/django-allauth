63d4729491abe6abd7a5bb789eed7e7cdd9de6b8
rulc <luka.todorovic.boznar@gmail.com>
2021-05-25 15:32:57 +0200

    Change app label

---
+++ allauth/socialaccount/providers/instagram/__init__.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class InstagramConfig(AppConfig):
+    name = 'allauth.socialaccount.providers.instagram'
+    label = 'allauth_instagram'
+
+
+default_app_config = 'allauth.socialaccount.providers.instagram.InstagramConfig'
